a83bff23fa6896b5e802fda01d8fb5e954a2a17e
casjay <casjay@yahoo.com>
2020-09-09 16:02:07 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
-mkdir -p /srv/docker/postgres && chmod -Rf 777 /srv/docker/postgres
+APPNAME="postgresql"
+DATADIR="/srv/docker/$APPNAME"
+
+mkdir -p "$DATADIR" && chmod -Rf 777 "$DATADIR"
+
+if docker ps -a | grep "$APPNAME" >/dev/null 2>&1; then
+docker pull postgres && docker restart $APPNAME
+else
 
 docker run -d \
 --name postgres \
 --restart=always \
 -p 5432:5432 \
--v /srv/docker/postgres:/var/lib/postgresql/data \
+-v $DATADIR:/var/lib/postgresql/data \
 postgres
+fi
