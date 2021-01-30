8d51750b1e549bf25b86f59e19d5e2e5fc56d975
Jason <jason@casjay.net>
2021-01-29 23:28:02 -0500

    🦈🏠❗ Well I did things - Hopefully nothing breaks ❗🏠🦈

--- install.sh
+++ install.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
 APPNAME="postgresql"
-DATADIR="/srv/docker/$APPNAME"
 
 mkdir -p "$DATADIR" && chmod -Rf 777 "$DATADIR"
 
 if docker ps -a | grep "$APPNAME" >/dev/null 2>&1; then
-docker pull postgres && docker restart $APPNAME
+  docker pull postgres && docker restart "$APPNAME"
 else
 
-docker run -d \
---name postgres \
---restart=always \
--p 5432:5432 \
--v $DATADIR:/var/lib/postgresql/data \
-postgres
+  docker run -d \
+    --name postgres \
+    --restart=always \
+    -p 5432:5432 \
+    -v "$DATADIR":/var/lib/postgresql/data \
+    postgres
 fi
