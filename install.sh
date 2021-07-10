13303304b49b89c6474afc8cb2a641fb584a4bb0
Jason <jason@casjay.net>
2021-07-10 15:54:55 -0400

    🐛 FIXED: install.sh 🐛

--- install.sh
+++ install.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
 APPNAME="postgresql"
+DOCKER_HUB_URL="postgres"
 
-mkdir -p "$DATADIR" && chmod -Rf 777 "$DATADIR"
+sudo mkdir -p "$DATADIR"
+sudo chmod -Rf 777 "$DATADIR"
 
 if docker ps -a | grep "$APPNAME" >/dev/null 2>&1; then
-  docker pull postgres && docker restart "$APPNAME"
+  sudo docker pull "$DOCKER_HUB_URL"
+  sudo docker restart "$APPNAME"
 else
 
-  docker run -d \
+  sudo docker run -d \
     --name postgres \
     --restart=always \
     -p 5432:5432 \
     -v "$DATADIR":/var/lib/postgresql/data \
-    postgres
+    "$DOCKER_HUB_URL"
 fi
